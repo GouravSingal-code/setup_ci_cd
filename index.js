516977850b322e7a8eed950e53aa00dfae2492b7
GouravSingal-code <gouravsingal2003@gmail.com>
2025-03-18 17:12:16 +0530

    change the log message

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("Hello, CI/CD PipelFERine!");
+    res.send("Hello, CI/CD Pipeline!");
 });
 
 const PORT = process.env.PORT || 3000;
