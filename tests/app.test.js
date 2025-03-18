e490b0c73d9474a5e06333ad2f16b60c9b3605bb
GouravSingal-code <gouravsingal2003@gmail.com>
2025-03-18 17:15:15 +0530

    change

--- tests/app.test.js
+++ tests/app.test.js
@@ -2,7 +2,7 @@ const request = require("supertest");
 const { app, server } = require("../index");
 
 describe("GET /", () => {
-    it("should return 'Hello, CI/CD Pipeline!'", async () => {
+    it("should return 'Hello, CI/CD Pipelined!'", async () => {
         const res = await request(app).get("/");
         expect(res.statusCode).toBe(200);
         expect(res.text).toBe("Hello, CI/CD Pipeline!");
