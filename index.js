4c1d7bc3b6296e96ffb2f28af6d84e21f4b85429
GouravSingal-code <gouravsingal2003@gmail.com>
2025-03-18 16:51:00 +0530

    refactor test cases

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("Hello, CI/CD Pipeadawrrfline!");
+    res.send("Hello, CI/CD Pipeline!");
 });
 
 const PORT = process.env.PORT || 3000;
